a397e925c5ffb8d6da67cf7fe6097f0dc453a19d
Jeremy Miller <jeremydmiller@yahoo.com>
2012-08-02 08:35:16 -0500

    bottle configuration and rake support

--- rakefile.rb
+++ rakefile.rb
@@ -81,6 +81,9 @@ end
 
 desc "Compiles the app"
 task :compile => [:restore_if_missing, :clean, :version] do
+  FileUtils.rm_rf 'src/SlickGridHarness/fubu-content'
+  bottles("assembly-pak src/FubuMVC.SlickGrid")
+
   MSBuildRunner.compile :compilemode => COMPILE_TARGET, :solutionfile => 'src/FubuMVC.SlickGrid.sln', :clrversion => CLR_TOOLS_VERSION
 
   target = COMPILE_TARGET.downcase
