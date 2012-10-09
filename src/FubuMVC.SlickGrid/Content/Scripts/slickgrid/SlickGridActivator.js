2ea93da630f352f8444391ebdeb2fbd6062b103d
jeremydmiller <jeremydmiller@yahoo.com>
2012-10-09 12:57:11 -0500

    successful spike of the underscore template

--- src/FubuMVC.SlickGrid/Content/Scripts/slickgrid/SlickGridActivator.js
+++ src/FubuMVC.SlickGrid/Content/Scripts/slickgrid/SlickGridActivator.js
@@ -201,3 +201,41 @@ function makeSlickGrid(div) {
 
 
 }
+
+
+
+(function ($) {
+    var templates = {};
+
+    var getTemplate = function (subject) {
+        if (templates[subject] == null) {
+            var html = $('[data-subject=' + subject + ']').html();
+            templates[subject] = _.template(html);
+        }
+
+        return templates[subject];
+    }
+
+
+    function UnderscoreTemplateFormatter(row, cell, value, columnDef, dataContext) {
+        var subject = columnDef.subject;
+
+        return Slick.Templates.apply(subject, dataContext);
+    }
+
+    $.extend(true, window, {
+        "Slick": {
+            "Formatters": {
+                "UnderscoreTemplate": UnderscoreTemplateFormatter
+            },
+            "Templates": {
+                apply: function (subject, data) {
+                    return getTemplate(subject)(data);
+                }
+            }
+        }
+    });
+
+
+
+})(jQuery);
